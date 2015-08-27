c85aa72ff7c6bfc136b9c416a169c4e97365736a
Jeff Richardson <jeff@masterpieceinteractive.com>
2015-08-27 16:45:39 -0500

    first commit

---
+++ app/main.js
@@ -0,0 +1,34 @@
+angular.module('AddressBook', [])
+  .service("contactService", function($http) {
+    var self = this;
+    self.contacts = [];
+    $http.get("http://localhost:9001/contacts")
+    .then(function(res) {
+      console.log(res);
+      while(res.data[0]) {
+         self.contacts.push(res.data.pop());
+      }
+    });
+  })
+  .controller("ContactController", function(contactService, $scope) {
+    $scope.contacts = contactService.contacts;
+  })
+  .filter("proper", function() {
+    return function(name) {
+       var type = typeof name;
+       if (type !== 'number' && type !== 'string') { throw new Error(); }
+      
+       return name.toString().split(" ").map(function(word) {
+         return word[0].toUpperCase().concat(word.slice(1));
+       }).join(" ");
+    }
+  })
+  .directive("avatar", function() {
+    return {
+      restrict: "AE",
+      scope: {
+        name: "="
+      },
+      template: "<span class='avatar'>{{name[0] | proper}}</span>"
+    }
+  });
---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var cors = require('cors');
+
+var app = express();
+app.use(cors());
+
+var contacts = [{
+  name: "Eddard"
+}, {
+  name: "Robert"
+}, { 
+  name: "Jeff"
+}];
+
+app.get('/contacts', function(req, res) {
+  res.status(200)
+    .json(contacts);
+})
+
+app.listen(9001);
